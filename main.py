d402120be9f9fec57171b906ad0310ed94ae3898
Arnav Nagpurkar <84492896+ArnavNagpurkar@users.noreply.github.com>
2023-04-25 17:16:51 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
+import random
 
+passItems = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+             'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+             'Q', 'R', 'S', 'U', 'T', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '!',
+             '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '+', '=', '{', '}', '[', ']', '|', '\\', ';', ':',
+             "'", '"', ',', '<', '.', '>', '?', '/']
+
+passLen = int(input("Enter length of password that you want: "))
+
+passwordList = random.choices(passItems, k=passLen)
+password = ""
+for item in passwordList:
+    password += item
+print(f'Your {passLen} digit password is:\n{password}')
